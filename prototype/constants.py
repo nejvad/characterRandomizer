3e6a690c3ad8df402a8bd383b7bead32744a5545
nejvad <jenesesdavid620@gmail.com>
2024-07-04 05:52:13 +0200

    intorudced constans, determine undead or not

---
+++ prototype/constants.py
@@ -0,0 +1,21 @@
+GENDER = ["Male", "Female"]
+RACE = ["Human", "Elf", "Dwarf", "Lizard"]
+
+# 3 attributes
+ATTRIBUTES = ["Strength", "Finesse", "Intelligence",
+              "Constitution", "Memory", "Wits"]
+
+# 2 abilities
+ABILITIES = ["Dual Wielding", "Ranged", "Single-Handed", "Two-Handed", "Leadership", "Perserverance",
+             "Retribution", "Aerotheurge", "Geomancer", "Huntsman", "Hydrosophist", "Necromancer", "Polymorph",
+             "Pyrokinetic", "Scoundrel", "Summoning", "Warfare"]
+# 2 civil abilities
+CIVIL_ABILITIES = ["Telekinesis", "Loremaster", "Sneaking", "Thievery", "Bartering",
+                   "Persuasion", "Lucky Charm"]
+# 7 talents
+TALENTS = ["All Skilled Up", "Ambidextrous", "Arrow Recovery", "Bigger and better",
+           "Comeback Kid", "Demon", "Duck Duck Goose", "Elemental Affinity", "Escapist", "Executioner",
+           "Elemental Ranger", "Far Out Man", "Five-Star Diner", "Glass Cannon", "Guerrilla",
+           "Hothead", "Ice King", "Leech", "Living Armour", "Lonewolf", "Mnemonic", "Morning Person",
+           "Opportunist", "Parry Master", "Pet Pal", "Picture of Health", "Savage Sortilege", "Slingshot",
+           "Stench", "The Pawn", "Torturer", "Unstable", "Walk It Off", "What a Rush"]
--- prototype/dos2randomizer.py
+++ prototype/dos2randomizer.py
@@ -1,39 +1,23 @@
 from random import choice, randrange
-
-GENDER = ["Male", "Female"]
-RACE = ["Human", "Elf", "Dwarf", "Lizard"]
-
-# 3 attributes
-ATTRIBUTES = ["Strength", "Finesse", "Intelligence",
-              "Constitution", "Memory", "Wits"]
-
-# 2 abilities
-ABILITIES = ["Dual Wielding", "Ranged", "Single-Handed", "Two-Handed", "Leadership", "Perserverance",
-             "Retribution", "Aerotheurge", "Geomancer", "Huntsman", "Hydrosophist", "Necromancer", "Polymorph",
-             "Pyrokinetic", "Scoundrel", "Summoning", "Warfare"]
-# 2 civil abilities
-CIVIL_ABILITIES = ["Telekinesis", "Loremaster", "Sneaking", "Thievery", "Bartering",
-                   "Persuasion", "Lucky Charm"]
-# 7 talents
-TALENTS = ["All Skilled Up", "Ambidextrous", "Arrow Recovery", "Bigger and better",
-           "Comeback Kid", "Demon", "Duck Duck Goose", "Elemental Affinity", "Escapist", "Executioner",
-           "Elemental Ranger", "Far Out Man", "Five-Star Diner", "Glass Cannon", "Guerrilla",
-           "Hothead", "Ice King", "Leech", "Living Armour", "Lonewolf", "Mnemonic", "Morning Person",
-           "Opportunist", "Parry Master", "Pet Pal", "Picture of Health", "Savage Sortilege", "Slingshot",
-           "Stench", "The Pawn", "Torturer", "Unstable", "Walk It Off", "What a Rush"]
+import constants
 
 
 def main():
     for i in range(4):
-        attributes = [choice(ATTRIBUTES) for _ in range(3)]
-        abilities = [choice(ABILITIES) for _ in range(2)]
-        civil_abilities = [choice(CIVIL_ABILITIES) for _ in range(2)]
-        talents = [choice(TALENTS) for _ in range(7)]
-        gender = choice(GENDER) + " " + choice(RACE)
+        attributes = [choice(constants.ATTRIBUTES) for _ in range(3)]
+        abilities = [choice(constants.ABILITIES) for _ in range(2)]
+        civil_abilities = [choice(constants.CIVIL_ABILITIES) for _ in range(2)]
+        talents = [choice(constants.TALENTS) for _ in range(7)]
+        gender = choice(constants.GENDER) + " " + choice(constants.RACE)
+        undead = ""
+        number = randrange(0, 101)
+        if number > 50:
+            undead = "Undead"
 
         print(f"""
-            {i + 1}
-            Character: {gender}
+            Character {i + 1}
+            ==============================
+            Gender: {undead} {gender}
             Attributes: {", ".join(attributes)}
             Abilities: {", ".join(abilities)}
             Civil Abilities: {", ".join(civil_abilities)}
