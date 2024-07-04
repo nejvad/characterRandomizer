30359a373b6d1baf8c92445d8866e3047705a80b
nejvad <jenesesdavid620@gmail.com>
2024-07-04 04:50:19 +0200

    added possible character attributes

--- prototype/dos2randomizer.py
+++ prototype/dos2randomizer.py
@@ -1,3 +1,14 @@
+GENDER = ["Male", "Female"]
+RACE = ["Human", "Elf", "Dwarf", "Lizard"]
+ATTRIBUTES = ["Strength", "Finesse", "Intelligence",
+              "Constitution", "Memory", "Wits"]
+ABILITIES = ["Dual Wielding", "Ranged", "Single-Handed", "Two-Handed", "Leadership", "Perserverance",
+             "Retribution", "Aerotheurge", "Geomancer", "Huntsman", "Hydrosophist", "Necromancer", "Polymorph",
+             "Pyrokinetic", "Scoundrel", "Summoning", "Warfare"]
+CIVIL_ABILITIES = ["Telekinesis", "Loremaster", "Sneaking", "Thievery", "Bartering",
+                   "Persuasion", "Lucky Charm"]
+
+
 def main():
     return ""
 
