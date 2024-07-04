b47bc9cc3e406accefc8aa6bb327fad75303e95f
nejvad <jenesesdavid620@gmail.com>
2024-07-04 06:07:27 +0200

    function to remove duplicate stats

--- prototype/dos2randomizer.py
+++ prototype/dos2randomizer.py
@@ -4,10 +4,10 @@ import constants
 
 def main():
     for i in range(4):
-        attributes = [choice(constants.ATTRIBUTES) for _ in range(3)]
-        abilities = [choice(constants.ABILITIES) for _ in range(2)]
-        civil_abilities = [choice(constants.CIVIL_ABILITIES) for _ in range(2)]
-        talents = [choice(constants.TALENTS) for _ in range(7)]
+        attributes = draw_character_stats(constants.ATTRIBUTES, 3)
+        abilities = draw_character_stats(constants.ABILITIES, 2)
+        civil_abilities = draw_character_stats(constants.CIVIL_ABILITIES, 2)
+        talents = draw_character_stats(constants.TALENTS, 7)
         gender = choice(constants.GENDER) + " " + choice(constants.RACE)
         undead = ""
         number = randrange(0, 101)
@@ -23,6 +23,17 @@ def main():
             Civil Abilities: {", ".join(civil_abilities)}
             Talents: {", ".join(talents)} 
         """)
+
+
+def draw_character_stats(array, amount):
+    picked_stats = []
+
+    while len(picked_stats) != amount:
+        current_stat = choice(array)
+        if current_stat not in picked_stats:
+            picked_stats.append(current_stat)
+
+    return picked_stats
 
 
 if __name__ == "__main__":
