8efa51410066fc16ef9a1b5134c6568798c10ca1
nejvad <jenesesdavid620@gmail.com>
2024-10-17 00:25:25 +0200

    changed gender, race to String type

--- src/main/java/org/randomizer/model/Character.java
+++ src/main/java/org/randomizer/model/Character.java
@@ -3,8 +3,8 @@ package org.randomizer.model;
 import java.util.List;
 
 public class Character {
-    private Gender gender;
-    private Race race;
+    private String gender;
+    private String race;
     private boolean isUndead;
     private List<String> attributes;
     private List<String> abilities;
