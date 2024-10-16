08dab37a21cb7ff1fb3ca9ef547dcb2ab4914fd1
nejvad <jenesesdavid620@gmail.com>
2024-10-17 01:41:24 +0200

    simple main loop

--- src/main/java/org/randomizer/Main.java
+++ src/main/java/org/randomizer/Main.java
@@ -1,7 +1,30 @@
 package org.randomizer;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        System.out.println("Welcome!");
+        System.out.println("Generate Characters (g)");
+        System.out.println("Exit program (e)");
+
+        boolean run = true;
+        while(run) {
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(System.in));
+
+            String amount = reader.readLine();
+
+            if (amount.equals("g")) {
+                System.out.println("Character creation");
+            } else if (amount.equals("e")) {
+               run = false;
+            } else {
+                System.out.println("Invalid Input!");
+            }
+
+        }
     }
 }
