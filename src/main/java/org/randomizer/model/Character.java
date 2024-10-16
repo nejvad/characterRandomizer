2da47f62faf770f9a19999292962c18a0d2ec282
nejvad <jenesesdavid620@gmail.com>
2024-10-17 00:21:52 +0200

    created empty Character class

---
+++ src/main/java/org/randomizer/model/Character.java
@@ -0,0 +1,13 @@
+package org.randomizer.model;
+
+import java.util.List;
+
+public class Character {
+    private Gender gender;
+    private Race race;
+    private boolean isUndead;
+    private List<String> attributes;
+    private List<String> abilities;
+    private List<String> civilAbilities;
+    private List<String> talents;
+}
